6ebd3c053d1463ceaabf9176d478d80c0c60c724
Natalie Riquelmy <n.riquelmy@gmail.com>
2022-04-14 17:09:10 -0400

    origins added to crossorigin annotation, and search component now rendering information from API in browser

--- java/src/main/java/com/techelevator/services/MarvelComicService.java
+++ java/src/main/java/com/techelevator/services/MarvelComicService.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins="*")
 @Component
 public class MarvelComicService {
 
--- vue/src/components/Search.vue
+++ vue/src/components/Search.vue
@@ -6,41 +6,22 @@
        <select v-model="searchCategory">
               <option v-for="category in searchCategories" v-bind:key="category" :value="category">{{category}}</option>
         </select>
-       <button>Search</button> 
-       <h3 v-for="comic in filteredComics" v-bind:key="comic" style="color: white;">{{comic.title}}||{{comic.author}}</h3>   
+       <button v-on:click="searchCollection()">Search</button> 
+       <h3 v-for="comic in searchResults" v-bind:key="comic.id" style="color: white;">{{comic.comicTitle}}||{{comic.imageURL}}</h3>    -->
+
+     
       </div>
   </div>
 </template>
 
 <script>
 import ComicCollectionService from "@/services/ComicCollectionService.js"
+
 export default {
   data() {
         return {
-           /*  collections: [ //placeholder
-                {
-                    name: "Deadpool Collection",
-                    public: true
-                },
-                {
-                    name: "Supermen Who Aren't Superman",
-                    public: false
-                }
-            ],
-            comics: [
-                {
-                    title: "Marvelous Marvel",
-                    author: "Neil Armstrong"
-                },
-                {
-                    title: "The Amazing Spider-Man #1",
-                    author: "Stan Lee"
-                },
-                {
-                    title: "The Fantastic Four #4",
-                    author: "Jack Kirby"
-                }
-            ], */
+           
+            searchResults: [],
             search: "",
             searchCategories: [
                 "General",
@@ -57,33 +38,35 @@ export default {
            //insert them into the this.comics array
            ComicCollectionService.getComics().then(response=>{
              this.$store.commit("SET_COMICS", response.data)
-           })
+           });
+       },
+       searchCollection(){
+
+            alert(this.$store.state.comics.length);
+            alert(this.search);
+
+            //this.collectionComics.forEach(comic => alert(comic.comicTitle));
+
+           this.searchResults = this.$store.state.comics.filter((comic) => {
+                return (comic.comicTitle).toLowerCase().includes((this.search).toLowerCase());
+                }
+           );
+
+           alert(this.searchResults.length + " in results");
        }
+
    },
    computed: {
-       filteredComics() {
-           let filtered = this.comics;
+       collectionComics() {
            //Filtering based solely off of comic name and author.
            //Other fields can be added later
-            if(this.search != ""){
-                filtered = [];
-                this.comics.forEach((comic) => {
-                    let chosen = false;
-                    if(comic.title.toLowerCase().includes(this.search.toLowerCase()) && !chosen){
-                        filtered.push(comic);
-                        chosen = true;
-                    }
-                    if(comic.author.toLowerCase().includes(this.search.toLowerCase()) && !chosen){
-                        filtered.push(comic);
-                        chosen = true;
-                    }
-                });
-            }
 
-            return filtered;
-        //    return this.comics.filter(comic => {
-        //        return comic.author.toLowerCase().indexOf(this.search.toLowerCase()) != -1;
-        //    });
+
+
+           
+
+            return this.$store.state.comics;
+        
        }
   },
   created (){
