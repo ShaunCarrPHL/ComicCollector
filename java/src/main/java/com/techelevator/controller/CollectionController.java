f94ea949065551dc0432816a9c2698a7ba1dc3f9
J. Luke Schlosser <coolhandjls@gmail.com>
2022-04-15 15:50:22 -0400

    create collection/add comic complete

--- java/src/main/java/com/techelevator/controller/CollectionController.java
+++ java/src/main/java/com/techelevator/controller/CollectionController.java
@@ -3,12 +3,10 @@ package com.techelevator.controller;
 import com.techelevator.dao.CollectionDao;
 import com.techelevator.dao.ComicDao;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin
-//@RestController
+@CrossOrigin
+@RestController
 public class CollectionController {
 
     private CollectionDao collectionDao;
@@ -16,6 +14,18 @@ public class CollectionController {
 
     String Api_Base_URL = "http://gateway.marvel.com/v1/public";
  // create collection, add comic to collection, update collection ie rename collection
-    //@RequestMapping(HttpStatus.CREATED)
+
+    //should this be a boolean?
+    @ResponseStatus
+    @RequestMapping ( path = "/collection/", method = RequestMethod.POST)
+    public int createCollection(@RequestParam String collectionName, int userId) throws Exception {
+        return collectionDao.createCollection(collectionName, userId);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping( path = "/collection/{collectionId}", method = RequestMethod.POST)
+    public boolean addComicToCollection(@RequestParam int marvelId, String comicTitle, String imageUrl, String description, int collectionId) throws Exception {
+        return collectionDao.addComicToCollection(marvelId, comicTitle, imageUrl, description, collectionId);
+    }
 
 }
