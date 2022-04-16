bba05c254e4020caf8a54d77a7c606b8f5ab6753
Shaun Carr <Shaunrcarr@gmail.com>
2022-04-16 15:36:42 -0400

    dummy data

--- java/database/dummydata.sql
+++ java/database/dummydata.sql
@@ -3,3 +3,16 @@ INSERT INTO comic(marvel_id, image, title, description)
 	RETURNING comic_id;
 	
 	
+	INSERT INTO collection(collection_id, collection_name, user_id, private)
+	VALUES (999,'cool collection',3,FALSE);
+
+	
+	SELECT * FROM collection;
+	
+	SELECT * FROM collection WHERE  = 999;
+	SELECT * FROM collection WHERE user_id = 3;
+	SELECT * FROM collection WHERE collection_id = 999;
+	
+	INSERT INTO collection(collection_name,user_id)
+                VALUES('calm collection',3) RETURNING collection_id;
+	
