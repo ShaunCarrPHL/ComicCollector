32f598e067b65a7b3fa0d6a802d46260a88e5c60
J. Luke Schlosser <coolhandjls@gmail.com>
2022-04-18 19:11:46 -0400

    jdbctests added

--- java/src/test/java/com/techelevator/dao/JdbcCollectionDaoTests.java
+++ java/src/test/java/com/techelevator/dao/JdbcCollectionDaoTests.java
@@ -1,7 +1,6 @@
 package com.techelevator.dao;
 
 import com.techelevator.model.Collection;
-import com.techelevator.model.Comic;
 import com.techelevator.model.ComicDTO;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,9 +11,9 @@ import java.util.List;
 
 public class JdbcCollectionDaoTests extends BaseDaoTests{
 
-    private static final Collection COLLECTION_1 = new Collection(2, "testCollection1", 4, true);
-    private static final Collection COLLECTION_2 = new Collection(3, "testCollection2", 5, false);
-    private static final Collection COLLECTION_3 = new Collection(4, "testCollection3", 6, true);
+    private static final Collection COLLECTION_1 = new Collection(1, "testCollection1", 1, true);
+    private static final Collection COLLECTION_2 = new Collection(2, "testCollection2", 2, false);
+    private static final Collection COLLECTION_3 = new Collection(3, "testCollection3", 3, true);
 
     private Collection testCollection;
     private ComicDTO testComicDTO;
@@ -29,11 +28,11 @@ public class JdbcCollectionDaoTests extends BaseDaoTests{
 
     @Test
     public void getCollectionById_returns_correct_collection_for_id(){
-        Collection collection = sut.getCollectionById(2);
+        Collection collection = sut.getCollectionById(1);
         assertCollectionsMatch(COLLECTION_1, collection);
 
 
-        collection = sut.getCollectionById(3);
+        collection = sut.getCollectionById(1);
         assertCollectionsMatch(COLLECTION_2, collection);
     }
 
@@ -45,33 +44,33 @@ public class JdbcCollectionDaoTests extends BaseDaoTests{
 
     @Test
     public void getCollectionByUserId_returns_correct_collection_for_id(){
-        List<Collection> collections = sut.getCollectionsByUserId(4);
-        Assert.assertEquals(2, collections.size());
+        List<Collection> collections = sut.getCollectionsByUserId(1);
+        Assert.assertEquals(1, collections.size());
         assertCollectionsMatch(COLLECTION_1, collections.get(0));
         assertCollectionsMatch(COLLECTION_3, collections.get(0));
 
-        collections = sut.getCollectionsByUserId(5);
-        Assert.assertEquals(3, collections.size());
+        collections = sut.getCollectionsByUserId(2);
+        Assert.assertEquals(2, collections.size());
         assertCollectionsMatch(COLLECTION_2, collections.get(0));
     }
 
     public void updated_collection_has_expected_values_when_retrieved(){
-        Collection collectionToUpdate = sut.getCollectionById(2);
+        Collection collectionToUpdate = sut.getCollectionById(1);
 
         collectionToUpdate.setCollectionName("Hello I am updated");
         collectionToUpdate.setUserId(4);
         collectionToUpdate.setPrivate(false);
         sut.updateCollection(collectionToUpdate);
 
-        Collection retrieveCollection = sut.getCollectionById(2);
+        Collection retrieveCollection = sut.getCollectionById(1);
         assertCollectionsMatch(collectionToUpdate, retrieveCollection);
     }
 
     @Test
     public void addComicToCollection_returns_expected_value(){
-        sut.addComicToCollection(122, "Oh neat a comic", "imageURLTHINGY", "This description is a description", 4);
+        sut.addComicToCollection(122, "Oh neat a comic", "imageURLTHINGY", "This description is a description", 1);
 
-        List<Collection> collections = sut.getCollectionsByUserId(4);
+        List<Collection> collections = sut.getCollectionsByUserId(1);
         Assert.assertEquals(1, collections.size());
         assertCollectionsMatch(COLLECTION_1, collections.get(0));
     }
